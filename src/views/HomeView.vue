7b790b06c68839c08331a4296c165a295605ec14
Alejandro Jauregui <alejandrojauregui12@yahoo.es>
2022-07-14 16:39:59 -0400

    configuracion inicial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+  </div>
+ 
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
